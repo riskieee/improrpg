7f817736943698f787f9af511514884f4d106464
riskieee <git@riskieee.de>
2021-05-27 13:02:46 +0200

    Fixed style + bug issues according eslint

--- src/story.js
+++ src/story.js
@@ -1,16 +1,22 @@
 // main Story class for improRPG
 
 class Story {
+
   constructor(name, theme) {
+
     this.name = name
     this.theme = theme
     this.participants = []
     this.lastEdit = 0
     this.content = []
+
   }
 
   get info() {
+
     return `
+      STORY INFO
+      ----------------------------------
       This is:     ${this.name}
       about:       ${this.theme} ⛺
       player:      ${this.participants.length}
@@ -18,29 +24,40 @@ class Story {
       last played: ${this.lastEdit} ⏲
     `
   }
+
   set info(newValue) {
-    throw new Error(
-      `A Story.info is only a getter. You can't override infos it with ${newValue}.`
-    );
+
+    // throw new Error(`A Story.info is only a getter. You can't override infos it with ${newValue}.`);
+
   }
 
-  addContentText(text, person) {
-    this.content.push([text, person])
+  addContent(text, person) {
+    const contentNode = {
+      content: text,
+      author: person,
+      time: ""
+    }
+    this.content.push(contentNode);
   }
-  _
-  addContentDecision(option, person) {
-    this.content.push(option)
-    this.participants.push(person)
-  }
-  _
+
+  //  addContentDecision(option, person) {
+  //    this.content.push(option)
+  //    this.participants.push(person)
+  //  }
+
   addParticipant(person) {
+
     this.participants.push(person)
+
   }
 
   get printStory() {
-    return this.name + '\n' +
-      this.content.map((contentNode) => contentNode.map((contetItem) => contetItem))
+
+    return `Story: ${this.name} ${'\n'}
+    .... here comes the Storycontent .... later ${'\n'}
+    .... ${'\n'}`  // + this.content.map((contentNode) => typeof(contentNode))
   }
+
 }
 
 module.exports = Story;
