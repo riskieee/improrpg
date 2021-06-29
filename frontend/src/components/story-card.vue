1bbe6c724f573818556f187c49e5d42e3357b2ae
riskieee <git@riskieee.de>
2021-06-29 15:30:39 +0200

    Fixing errors and layout improvement

--- frontend/src/components/story-card.vue
+++ frontend/src/components/story-card.vue
@@ -19,12 +19,10 @@ export default {
   .card.shadow-sm
     img.card-img-top(width='100%' height='225' :src='`/img/${ story.storyCover }`' alt='Storycover' aria-label='Storycover' preserveaspectratio='xMidYMid slice' focusable='false')
     .card-body
-      p.card-text
-        h5 {{ story.storyName }}
-          span {{ story.storyTheme }}
-          //- span(v-for="themes in story.storyTheme")  // should work
-        //-  {{ story.contentNodes.map(item => item.contentNode).join(' ') }}
-        //-  {{ story.contentNodes.map(player => player.addingPlayer.playerName).join(', ') }}
+      h5 {{ story.storyName }}
+        p.card-text
+          span(v-for="theme in story.storyTheme")
+            span.theme {{ theme }}
       .d-flex.justify-content-between.align-items-center
         .btn-group
           router-link.btn.btn-sm.btn-outline-primary.px-3(type='button' :to="`/stories/${story._id}`") Read
@@ -32,22 +30,14 @@ export default {
           //- Math.floor((Math.abs(date2-date1))/(1000*60*60*24))
         //- small.text-muted {{  timePast }} {{ story.lastEdit }} mins
         //- small.text-muted {{ story.lastEdit }} mins
-//- OLD REMOVE
-  //- img.card-img-top.py-1(src=`../assets/${story.storyCover || defaultCover}` width='100%' height='225' alt='cover' aria-label='cover' preserveaspectratio='xMidYMid slice' focusable='false')
-//- h2 Length ({{ story.contentNodes.length }})
-//- .photo(v-for="contentNode in story.contentNodes")
-//-   h3 {{ contentNodes.addingPlayer }}
-//-   p(v-if="!contentNodes.length")
-//-     | Story didn't started yet!
-//-   p(v-else)
-//-     | {{ photo.likedBy.map(user => user.name).join(', ') }}
+
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h5 span {
-  padding-left: 1rem;
-  color: #999;
+span.theme {
+  padding-left: 0.3rem;
+  color: #aaa;
   font-size: 0.9rem;
 }
 
--- frontend/src/views/story-detail.vue
+++ frontend/src/views/story-detail.vue
@@ -5,7 +5,6 @@ import { mapActions } from 'vuex'
 
 export default {
   name: 'StoryDetail',
-  props: ['story'],
   data() {
     return {
       story: null,
@@ -14,6 +13,7 @@ export default {
   },
   async created() {
     this.story = await this.fetchStory(this.$route.params.id)
+    console.log(this.story)
   },
   methods: {
     ...mapActions(['fetchStory'])
@@ -22,156 +22,120 @@ export default {
 </script>
 
 <template lang="pug">
+div
+  .container(v-if="!story") no story yet
+  .container(v-else)
 
-.container
-  // Page header start
-  //- .page-title
-  //-   .row.gutters
-  //-     .col-xl-6.col-lg-6.col-md-6.col-sm-12.col-12
-  //-       h5.title imroRPG
-  //-     .col-xl-6.col-lg-6.col-md-6.col-sm-12.col-12
-  // Page header end
-  // Content wrapper start
-  .content-wrapper
-    // Row start
-    .row.gutters
-      .col-xl-12.col-lg-12.col-md-12.col-sm-12.col-12
-        .card.m-0
-          // Row start
-          .row.no-gutters
-            .col-xl-4.col-lg-4.col-md-4.col-sm-3.col-3
-              .users-container
-                .chat-search-box
-                  .input-group
-                    input.form-control(placeholder='Search')
-                    .input-group-btn
-                      button.btn.btn-info(type='button')
-                        i.fa.fa-search
-                ul.users
-                  li.person(data-chat='person1')
-                    .user
-                      img(src='/img/avatar/avatar3.png' alt='Retail Admin')
-                      span.status.busy
-                    p.name-time
-                      span.name Luphus
-                      span.time 15/09/2021
-                  li.person(data-chat='person1')
-                    .user
-                      img(src='/img/avatar/avatar1.png' alt='Retail Admin')
-                      span.status.offline
-                    p.name-time
-                      span.name Erion
-                      span.time 15/09/2021
-                  li.person.active-user(data-chat='person2')
-                    .user
-                      img(src='/img/avatar/avatar2.png' alt='Retail Admin')
-                      span.status.away
-                    p.name-time
-                      span.name Selfil
-                      span.time 12/09/2021
-                  li.person(data-chat='person3')
-                    .user
-                      img(src='/img/avatar/avatar4.png' alt='Retail Admin')
-                      span.status.busy
-                    p.name-time
-                      span.name Dharzeth
-                      span.time 11/09/2021
-                  li.person(data-chat='person4')
-                    .user
-                      img(src='/img/avatar/avatar7.png' alt='Retail Admin')
-                      span.status.offline
-                    p.name-time
-                      span.name Lisla
-                      span.time 08/09/2021
-                  li.person(data-chat='person5')
-                    .user
-                      img(src='/img/avatar/avatar5.png' alt='Retail Admin')
-                      span.status.away
-                    p.name-time
-                      span.name Michelangelo
-                      span.time 05/09/2021
-            .col-xl-8.col-lg-8.col-md-8.col-sm-9.col-9
-              .selected-user
-                span
-                  span.name {{ story.storyName }} &nbsp;
-                  span ({{ story.storyTheme.join(', ') }})
+    // Page header start
+    //- .page-title
+    //-   .row.gutters
+    //-     .col-xl-6.col-lg-6.col-md-6.col-sm-12.col-12
+    //-       h5.title imroRPG
+    //-     .col-xl-6.col-lg-6.col-md-6.col-sm-12.col-12
+    // Page header end
+    // Content wrapper start
+    .content-wrapper
+      // Row start
+      .row.gutters
+        .col-xl-12.col-lg-12.col-md-12.col-sm-12.col-12
+          .card.m-0
+            // Row start
+            .row.no-gutters
+              .col-xl-4.col-lg-4.col-md-4.col-sm-3.col-3
+                .users-container
+                  .chat-search-box
+                    .input-group
+                      input.form-control(placeholder='Search')
+                      .input-group-btn
+                        button.btn.btn-info(type='button')
+                          i.fa.fa-search
+                  ul.users
+                    li.person(v-for="node in story.contentNodes")
+                      .user
+                        img(:src='`/img/avatar/${node.addingPlayer.playerPhoto}`' :title='`${node.addingPlayer.playerName }`' :alt='`${node.addingPlayer.playerName }`')
+                        //- span.status.busy // .offline // .away
+                      p.name-time
+                        span.name {{node.addingPlayer.playerName }}
+                      //-   span.time 15/09/2021
 
-              .chat-container
-                ul.chat-box.chatContainerScroll
-                  li.chat-left
-                    .chat-avatar
-                      img(src='/img/avatar/avatar3.png' alt='Retail Admin')
-                      .chat-name Luphus
-                    .chat-text
-                      | Your uncle asked you to bring a tied package to his br0other in the next village. Your journey begins today. You have packed a backpack with food, a change clothes, some tools, a knife, a blanket, rope and a tent. The weather is good and you set off.
-                    .chat-hour
-                      | 08:55
-                      span.fa.fa-check-circle
-                  li.chat-right
-                    .chat-hour
-                      | 08:56
-                      span.fa.fa-check-circle
-                    .chat-text
-                      | Todays path is relaxed and leads along the forest.You make good progress.You come to a crossroad and have to choose between several paths.
-                    .chat-avatar
-                      img(src='/img/avatar/avatar3.png' alt='Retail Admin')
-                      .chat-name Luphus
-                  li.chat-left
-                    .chat-avatar
-                      img(src='/img/avatar/avatar3.png' alt='Retail Admin')
-                      .chat-name Luphus
-                    .chat-text They are 5 paths. What they be? You look around and see 5 Options.
-                    .chat-hour
-                      | 08:57
-                      span.fa.fa-check-circle
-                  li.chat-right
-                    .chat-hour
-                      | 08:59
-                      span.fa.fa-check-circle
-                    .chat-text Well I am not sure. They may be a dark dangerous forest?
-                    .chat-avatar
-                      img(src='/img/avatar/avatar5.png' alt='Retail Admin')
-                      .chat-name Michelangelo
-                  li.chat-left
-                    .chat-avatar
-                      img(src='/img/avatar/avatar3.png' alt='Retail Admin')
-                      .chat-name Russell
-                    .chat-text Ok! Lets go for a angerous forest! Anybody else wit another path?
-                    .chat-hour
-                      | 08:57
-                      span.fa.fa-check-circle
-                  li.chat-right
-                    .chat-hour
-                      | 08:59
-                      span.fa.fa-check-circle
-                    .chat-text Hob about back to the village? Maybe to ask for some guardians?
-                    .chat-avatar
-                      img(src='/img/avatar/avatar4.png' alt='Retail Admin')
-                      .chat-name Jin
-                  li.chat-left
-                    .chat-avatar
-                      img(src='/img/avatar/avatar3.png' alt='Retail Admin')
-                      .chat-name Luphus
-                    .chat-text OK, why not. Back to the village for
-                    .chat-hour
-                      | 07:00
-                      span.fa.fa-check-circle
-                .form-group.mt-3.mb-0
-                  textarea.form-control(rows='3' placeholder='Type your storynotes here...')
-          // Row end
-    // Row end
-  // Content wrapper end
+              .col-xl-8.col-lg-8.col-md-8.col-sm-9.col-9
+                .selected-user
+                  span
+                    span.name {{ story.storyName }} &nbsp;
+                    span.theme ({{ story.storyTheme.join(', ') }})
+
+                .chat-container
+                  ul.chat-box.chatContainerScroll
+                    li.chat-left
+                      .chat-avatar
+                        img(src='/img/avatar/avatar3.png' alt='Retail Admin')
+                        .chat-name Luphus
+                      .chat-text
+                        | Your uncle asked you to bring a tied package to his br0other in the next village. Your journey begins today. You have packed a backpack with food, a change clothes, some tools, a knife, a blanket, rope and a tent. The weather is good and you set off.
+                      .chat-hour
+                        | 08:55
+                        span.fa.fa-check-circle
+                    li.chat-right
+                      .chat-hour
+                        | 08:56
+                        span.fa.fa-check-circle
+                      .chat-text
+                        | Todays path is relaxed and leads along the forest.You make good progress.You come to a crossroad and have to choose between several paths.
+                      .chat-avatar
+                        img(src='/img/avatar/avatar3.png' alt='Retail Admin')
+                        .chat-name Luphus
+                    li.chat-left
+                      .chat-avatar
+                        img(src='/img/avatar/avatar3.png' alt='Retail Admin')
+                        .chat-name Luphus
+                      .chat-text They are 5 paths. What they be? You look around and see 5 Options.
+                      .chat-hour
+                        | 08:57
+                        span.fa.fa-check-circle
+                    li.chat-right
+                      .chat-hour
+                        | 08:59
+                        span.fa.fa-check-circle
+                      .chat-text Well I am not sure. They may be a dark dangerous forest?
+                      .chat-avatar
+                        img(src='/img/avatar/avatar5.png' alt='Retail Admin')
+                        .chat-name Michelangelo
+                    li.chat-left
+                      .chat-avatar
+                        img(src='/img/avatar/avatar3.png' alt='Retail Admin')
+                        .chat-name Russell
+                      .chat-text Ok! Lets go for a angerous forest! Anybody else wit another path?
+                      .chat-hour
+                        | 08:57
+                        span.fa.fa-check-circle
+                    li.chat-right
+                      .chat-hour
+                        | 08:59
+                        span.fa.fa-check-circle
+                      .chat-text Hob about back to the village? Maybe to ask for some guardians?
+                      .chat-avatar
+                        img(src='/img/avatar/avatar4.png' alt='Retail Admin')
+                        .chat-name Jin
+                    li.chat-left
+                      .chat-avatar
+                        img(src='/img/avatar/avatar3.png' alt='Retail Admin')
+                        .chat-name Luphus
+                      .chat-text OK, why not. Back to the village for
+                      .chat-hour
+                        | 07:00
+                        span.fa.fa-check-circle
+                  .form-group.mt-3.mb-0
+                    textarea.form-control(rows='3' placeholder='Type your storynotes here...')
+            // Row end
+      // Row end
+    // Content wrapper end
 
 
 </template>
-
 <style scoped lang="scss" scoped>
 /* 1.Variables */
-
 /* Color Pallet  */
-
 /* Search Box*/
-
 .chat-search-box {
   border-radius: 3px 0 0 0;
   padding: 0.75rem 1rem;
@@ -312,9 +276,9 @@ export default {
 
 .selected-user {
   width: 100%;
-  padding: 0 15px;
+  padding: 15px;
   min-height: 64px;
-  line-height: 64px;
+
   border-bottom: 1px solid #e6ecf3;
   border-radius: 0 3px 0 0;
 }
@@ -325,6 +289,14 @@ export default {
 
 .selected-user span.name {
   font-weight: 700;
+}
+
+.selected-user span.theme {
+  display: block;
+  padding: 0;
+  margin: 0;
+  font-weight: normal;
+  font-size: 0.85rem;
 }
 
 .chat-container {
