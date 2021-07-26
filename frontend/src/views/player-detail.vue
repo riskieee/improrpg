78875ca134dc2a29a7df828823abbed8849ce7e2
riskieee <git@riskieee.de>
2021-07-26 22:11:50 +0200

    Update buttoncolor

--- frontend/src/views/player-detail.vue
+++ frontend/src/views/player-detail.vue
@@ -41,9 +41,9 @@ export default {
       h2 Live streams
       div(v-for="stream in liveStreams")
         p {{ stream }}
-        button.btn.btn-primary.m-2(@click="joinStream(stream)") Join Chat
+        button.btn.btn-warning.m-2(@click="joinStream(stream)") Join Chat
     div.bd-chatbox
-      button.btn.btn-primary.m-2(v-if="!currentLiveStream" @click="goLive") Start Chat
+      button.btn.btn-warning.m-2(v-if="!currentLiveStream" @click="goLive") Start Chat
       div(v-if="currentLiveStream")
         h3 Live stream
         .messages.container
@@ -53,7 +53,7 @@ export default {
               span.form-control {{ message.body }}
         .container
           form.input-group.mb-3(@submit="sendMessage")
-            button.btn.btn-primary(type="submit" value="Send message") SEND MESSAGE
+            button.btn.btn-warning(type="submit" value="Send message") SEND MESSAGE
             input.form-control.form-control(type="text" v-model="message")
     //- Counter
 </template>
